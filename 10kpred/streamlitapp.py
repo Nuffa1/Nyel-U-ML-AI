a398f42662987acb3c38092eb520fdfd2504c539
Nyel Uffa <102193415+Nuffa1@users.noreply.github.com>
2024-05-04 13:11:06 -0500

    g

--- 10kpred/streamlitapp.py
+++ 10kpred/streamlitapp.py
@@ -1,1 +1,8 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+import sklearn
+import streamlit as st
 
+st.title("Streamlit App")
