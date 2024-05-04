863769fbcf34354dcca502d1588794f7a6ca15de
Nyel Uffa <102193415+Nuffa1@users.noreply.github.com>
2024-05-04 13:15:53 -0500

    y

--- 10kpred/streamlitapp.py
+++ 10kpred/streamlitapp.py
@@ -5,4 +5,10 @@ from sklearn.preprocessing import StandardScaler
 import sklearn
 import streamlit as st
 
+data = pd.read_csv('newdata.csv')
+target = data.pop("winner")
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model.fit(data, target)
+model.predict(np.array([[3023,2501,5172,3026,2502,3871]]))
+
 st.title("Streamlit App")
