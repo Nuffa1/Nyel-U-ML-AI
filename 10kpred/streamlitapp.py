85a7295de8e154f4adb8de16a7d4f4d6c3c87767
Nyel Uffa <102193415+Nuffa1@users.noreply.github.com>
2024-05-04 13:57:31 -0500

    Added the prediction regression

--- 10kpred/streamlitapp.py
+++ 10kpred/streamlitapp.py
@@ -4,7 +4,15 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 import sklearn
 import streamlit as st
+from pathlib import Path
+
+datafile = Path(__file__).parents[1] / '10kpred/newdata.csv'
+
+data = pd.read_csv(datafile)
+
+target = data.pop("winner")
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
+model.fit(data, target)
+model.predict(np.array([[3023,2501,5172,3026,2502,3871]]))
 
 
-
-data = pd.read_csv('newdata.csv')
